83897eb2830bb884762a503c229ef41174a37c09
Zoritta <113990865+Zoritta@users.noreply.github.com>
2023-04-24 19:13:14 +0200

    is fullhouse

--- tests/isFullHouse.test.js
+++ tests/isFullHouse.test.js
@@ -13,7 +13,7 @@ test('check that isFullHouse returns Falsy if not FullHouse', () => {
 });
 
 test('check that isFullHouse returns a higher score for a stronger hand (if two hands but with FullHouse)', () => {
-  let hand1 = new Hand('♥9', '♦3', '♣7', '♥5', '♦6');
+  let hand1 = new Hand('♦9', '♦3', '♣7', '♥5', '♦6');
   let hand2 = new Hand('♥8', '♣8', '♥6', '♦6', '♠6');
   let hand1Score = CompareHands.isFullHouse(hand1);
   let hand2Score = CompareHands.isFullHouse(hand2);
