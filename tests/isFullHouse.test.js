5b89579a9d2c64f5279d3282979f5ab1c6742419
Zoritta <113990865+Zoritta@users.noreply.github.com>
2023-04-24 19:09:59 +0200

    is fullhouse test

---
+++ tests/isFullHouse.test.js
@@ -0,0 +1,21 @@
+const Hand = require('../Hand');
+const CompareHands = require('../CompareHands');
+const suits = '♥♦♣♠';
+
+test('check that isFullHouse returns Truthy if FullHouse', () => {
+  let hand = new Hand('♥8', '♣8', '♥6', '♦6', '♠6');
+  expect(CompareHands.isFullHouse(hand)).toBeTruthy();
+});
+
+test('check that isFullHouse returns Falsy if not FullHouse', () => {
+  let hand = new Hand('♥9', '♦3', '♣7', '♥5', '♦6');
+  expect(CompareHands.isFullHouse(hand)).toBeFalsy();
+});
+
+test('check that isFullHouse returns a higher score for a stronger hand (if two hands but with FullHouse)', () => {
+  let hand1 = new Hand('♥9', '♦3', '♣7', '♥5', '♦6');
+  let hand2 = new Hand('♥8', '♣8', '♥6', '♦6', '♠6');
+  let hand1Score = CompareHands.isFullHouse(hand1);
+  let hand2Score = CompareHands.isFullHouse(hand2);
+  expect(hand2Score).toBeGreaterThan(hand1Score);
+});
