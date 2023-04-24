e9b47096c5388c8640e24fd4a7d302eee33fa7ef
Zoritta <113990865+Zoritta@users.noreply.github.com>
2023-04-24 19:27:38 +0200

    is OnePair test

---
+++ tests/isOnePair.test.js
@@ -0,0 +1,22 @@
+const Hand = require('../Hand');
+const CompareHands = require('../CompareHands');
+const suits = '♥♦♣♠';
+
+test('check that isOnePair returns Truthy if OnePair', () => {
+  let hand = new Hand('♥8', '♣8', '♥7', '♦6', '♠5');
+  expect(CompareHands.isOnePair(hand)).toBeTruthy();
+});
+
+test('check that isOnePair returns Falsy if not OnePair', () => {
+  let hand = new Hand('♥9', '♦3', '♣7', '♥5', '♦6');
+  expect(CompareHands.isOnePair(hand)).toBeFalsy();
+});
+
+test('check that isonePair returns a higher score for a stronger hand (if two hands but with onePair)', () => {
+  let hand1 = new Hand('♦9', '♦3', '♣7', '♥5', '♦6');
+  let hand2 = new Hand('♥8', '♣8', '♥7', '♦6', '♠5');
+  let hand1Score = CompareHands.isOnePair(hand1);
+  let hand2Score = CompareHands.isOnePair(hand2);
+  expect(hand2Score).toBeGreaterThan(hand1Score);
+});
+
