4a8818c0828abd420b5cfee458ccbc04cc123ea1
Zoritta <113990865+Zoritta@users.noreply.github.com>
2023-04-24 18:14:15 +0200

    isStraight test

--- tests/isStraight.test.js
+++ tests/isStraight.test.js
@@ -2,7 +2,20 @@ const Hand = require('../Hand');
 const CompareHands = require('../CompareHands');
 const suits = '♥♦♣♠';
 
-test('check that isStraight returns truthy if straight', () => {
+test('check that isStraight returns Truthy if Straight', () => {
   let hand = new Hand('♥9', '♦8', '♣7', '♥5', '♦6');
   expect(CompareHands.isStraight(hand)).toBeTruthy();
+});
+
+test('check that isStraight returns Falsy if not Straight', () => {
+  let hand = new Hand('♣2', '♣6', '♥4', '♣8', '♣7');
+  expect(CompareHands.isStraight(hand)).toBeFalsy();
+});
+
+test('check that isStraight returns a higher score for a stronger hand (if two hands but with Straight)', () => {
+  let hand1 = new Hand('♣2', '♣6', '♣4', '♣8', '♣7');
+  let hand2 = new Hand('♥9', '♦8', '♣7', '♥5', '♦6');
+  let hand1Score = CompareHands.isStraight(hand1);
+  let hand2Score = CompareHands.isStraight(hand2);
+  expect(hand2Score).toBeGreaterThan(hand1Score);
 });
